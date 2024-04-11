ea6ba2a1bea7fd34cc9166722ce67cc8a886c1aa
RaymondKaruga <raymond.karuga@student.moringaschool.com>
2024-04-11 14:36:50 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+function updateDOM(){
+    document.getElementById("text").innerHTML="This is really cool!";
+  }
+  document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+   
+  });
